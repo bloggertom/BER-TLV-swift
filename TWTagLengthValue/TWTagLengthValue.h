d21fffcc7d516fc2ae5a1b2dd6526ef7113ebe47
Thomas Wilson <sabre.2win@gmail.com>
2015-11-21 20:26:13 +0000

    Initial Commit

---
+++ TWTagLengthValue/TWTagLengthValue.h
@@ -0,0 +1,19 @@
+//
+//  TWTagLengthValue.h
+//  TWTagLengthValue
+//
+//  Created by Thomas Wilson on 21/11/2015.
+//  Copyright © 2015 Thomas Wilson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TWTagLengthValue.
+FOUNDATION_EXPORT double TWTagLengthValueVersionNumber;
+
+//! Project version string for TWTagLengthValue.
+FOUNDATION_EXPORT const unsigned char TWTagLengthValueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TWTagLengthValue/PublicHeader.h>
+
+
